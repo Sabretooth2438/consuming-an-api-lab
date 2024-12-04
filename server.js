067d7b8f2cf5b894a1c0bc8f8c6472f7f380155b
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-12-04 14:05:20 +0300

    Project Finally is Done

--- server.js
+++ server.js
@@ -1,48 +1,84 @@
 const express = require('express')
 const axios = require('axios')
 const dotenv = require('dotenv')
+const moment = require('moment')
 
 dotenv.config()
 const app = express()
 
-// Middleware
+// Setup middleware and view engine
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-// Default port
 const PORT = process.env.PORT || 3000
-
-// API Key from .env
 const API_KEY = process.env.API_KEY
 
+// Routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/weather', async (req, res) => {
   const zip = req.body.zip
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=${API_KEY}`
+  const type = req.body.type
 
   try {
-    const response = await axios.get(apiUrl)
-    const weatherData = response.data
+    if (type === 'forecast') {
+      // Fetch 5-day forecast
+      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&units=imperial&appid=${API_KEY}`
+      const response = await axios.get(apiUrl)
+      const forecastData = response.data
 
-    // Pass data to the show page
-    res.render('weather/show', {
-      city: weatherData.name,
-      temp: weatherData.main.temp,
-      description: weatherData.weather[0].description
-    })
+      // Group forecast by date
+      const groupedForecast = {}
+      forecastData.list.forEach((item) => {
+        const date = item.dt_txt.split(' ')[0] // Extract the date part
+        if (!groupedForecast[date]) groupedForecast[date] = []
+        groupedForecast[date].push({
+          time: item.dt_txt.split(' ')[1], // Extract the time part
+          temp: item.main.temp,
+          description: item.weather[0].description,
+          icon: item.weather[0].icon
+        })
+      })
+
+      // Render grouped forecast data
+      res.render('weather/forecast', {
+        city: forecastData.city.name,
+        groupedForecast
+      })
+    } else if (type === 'current') {
+      // Fetch current weather
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=${API_KEY}`
+      const response = await axios.get(apiUrl)
+      const weatherData = response.data
+
+      // Render current weather data
+      res.render('weather/show', {
+        city: weatherData.name,
+        temp: weatherData.main.temp,
+        description: weatherData.weather[0].description,
+        icon: weatherData.weather[0].icon,
+        sunrise: moment.unix(weatherData.sys.sunrise).format('h:mm A'),
+        sunset: moment.unix(weatherData.sys.sunset).format('h:mm A'),
+        humidity: weatherData.main.humidity,
+        pressure: weatherData.main.pressure
+      })
+    } else {
+      res.render('error', {
+        message: 'Invalid weather type selected. Please try again.'
+      })
+    }
   } catch (error) {
-    res.status(500).send(`
-      <h1>Error</h1>
-      <p>Error fetching weather data. Please check the ZIP code and try again.</p>
-      <a href="/">Back to Home</a>
-    `)
+    res.render('error', {
+      message:
+        'Error fetching weather data. Please check the ZIP code and try again.'
+    })
   }
 })
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
